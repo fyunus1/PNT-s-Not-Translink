fe11ffb0756c5b5e80caae94b6289f8ce21d7720
Faiz Yunus <Faiz Yunus>
2016-10-15 14:30:44 -0700

    adding stops

--- pnt.pl
+++ pnt.pl
@@ -12,9 +12,31 @@ prop(line99, route, westbound99).
 prop(westbound99, stop, stop0_w99).
 prop(westbound99, stop, stop1_w99).
 %stop0_w99 is at boundary loop
-prop(stop0_w99, location, loughheed_highway_at_boundary_loop).
+prop(stop0_w99, location, loc(loughheed_highway,boundary_loop)).
 %stop1_w99 is at rupert st
-prop(stop1_w99, location, e_broadway_at_rupert_st).
+prop(stop1_w99, location, loc(e_broadway,rupert_st)).
+prop(stop2_w99, location, loc(e_broadway,renfrew_st)).
+prop(stop3_w99, location, loc(e_broadway,penticton_st)).
+prop(stop4_w99, location, loc(e_broadway,nanaimo_st)).
+prop(stop5_w99, location, loc(n_grandview_highway,commercial_dr)).
+prop(stop6_w99, location, loc(e_broadway,commercial_bay_2)).
+prop(stop7_w99, location, loc(e_broadway,clark_dr)).
+prop(stop8_w99, location, loc(e_broadway,fraser_st)).
+prop(stop9_w99, location, loc(e_broadway,main_st)).
+prop(stop10_w99, location, loc(w_broadway,cambie_st)).
+prop(stop11_w99, location, loc(w_broadway,willow_st)).
+prop(stop12_w99, location, loc(w_broadway,granville_st)).
+prop(stop13_w99, location, loc(w_broadway,arbutus_st)).
+prop(stop14_w99, location, loc(w_broadway,macdonald_st)).
+prop(stop15_w99, location, loc(alma_st,w_10_ave)).
+prop(stop16_w99, location, loc(w_broadway,alma_st)).
+prop(stop17_w99, location, loc(w_10_ave,sasamat_st)).
+prop(stop18_w99, location, loc(university_blvd, allison_rd)).
+prop(stop19_w99, location, loc(ubc_loop, bay4)).
+
+
+
+
 
 %westbound route has bus0
 prop(westbound99, bus, bus0_w99).
